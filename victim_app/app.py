7cfdae5c4239cae6c92b48919ead3a404a109bf5
Netchanok Yindee <tungpang.yindee2548@gmail.com>
2026-02-02 21:08:49 +0700

    create base web html

--- victim_app/app.py
+++ victim_app/app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template("home.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/comment")
+def main():
+    return render_template("comment.html")
+
+@app.route("/blocked_login")
+def blocked_login():
+    return "Access Denied: Login is blocked."
+
 if __name__ == "__main__":
     app.run(debug=True)
